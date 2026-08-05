980b1cdf9044f2c70a8391a0ef599ec20f1fa6d9
Frederick Ziola <frederick.ziola@gmail.com>
2026-08-04 23:30:25 -0400

    Enable nix-command and flakes

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,11 @@
       <home-manager/nixos>
     ];
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
