ec2ad3fd2e2d229a24c424d8dbb6d28643a6d616
FrederickAmpsUp <frederick.ziola@gmail.com>
2026-08-17 16:11:26 -0400

    Remove leading space from commit messages

--- dotfiles/nvim/lua/keymaps.lua
+++ dotfiles/nvim/lua/keymaps.lua
@@ -24,7 +24,7 @@ local function git_commit()
   vim.fn.prompt_setprompt(buf, "> ")
 
   local function commit()
-    local message = vim.fn.getline(1):sub(2)
+    local message = vim.fn.getline(1):sub(3)
 
     if message == "" then
       return
